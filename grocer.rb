d9a9db22e373a99b8cb23169a7e5d68e1c6ca2b6
Renée Cruz <reneebasscruz@gmail.com>
2019-07-27 02:34:33 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -26,6 +26,7 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
       
       if cart[item]
+        if cart[item][:count] >= coupon[:num]
         cart["#{item} W/COUPON"] ||= {}
         cart["#{item} W/COUPON"][:price] ||= coupon[:cost / coupon[:num] 
         
